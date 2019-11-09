9bad77470c50af7b516e53d962e1d67dba226354
manashmandal <manashmndl@gmail.com>
2019-11-09 22:56:25 +0600

    working so far

--- async_bitmex_ws.py
+++ async_bitmex_ws.py
@@ -3,10 +3,40 @@ import socketio
 from auth_util import BitMEXWS
 import asyncio
 import json
+from websockets.protocol import State
 
 apiKey = "F061zxLTOGYyPMlwq-EU4YaW"
 apiSecret = "uIOiJzEhhHK5oiC3v1M6nYskIlFKAdtg_Jc966EUsO7TqQPy"
 
 ws = BitMEXWS(apiKey=apiKey, secret=apiSecret, testnet=True)
 
-asyncio.get_event_loop().run_until_complete(ws.connect())
+
+async def printHello():
+    # print("hello")
+    # try:
+    while ws.state() == State.OPEN:
+        await asyncio.sleep(1)
+        print(ws.data)
+
+    await ws.close()
+
+    # if ws.state() == State.CLOSING or ws.state() == State.CLOSED:
+    #     await ws.close()
+
+
+async def run():
+    await ws.connect()
+    try:
+        await asyncio.gather(ws.receive_message(), printHello())
+    except Exception as exe:
+        print(str(exe), exe.__class__.__name__)
+
+    # print("hol up")
+    # await ws.connect()
+    # ws.receive_message()
+    # printHello()
+    # print("hey")
+
+
+# asyncio.get_event_loop().run_until_complete(ws.connect())
+asyncio.get_event_loop().run_until_complete(run())
--- auth_util.py
+++ auth_util.py
@@ -3,6 +3,21 @@ import urllib
 import hashlib
 import hmac
 import websockets
+from websockets.protocol import State
+import asyncio
+import json
+import math
+
+def find_by_keys(keys, table, matchData):
+    for item in table:
+        if all(item[k] == matchData[k] for k in keys):
+            return item
+
+
+def order_leaves_quantity(o):
+    if o["leavesQty"] is None:
+        return True
+    return o["leavesQty"] > 0
 
 
 class BitMEXWS:
@@ -33,6 +48,10 @@ class BitMEXWS:
         self.subscriptions = [
             f"{sub}:{self.symbol}" for sub in self.symbol_subs
         ] + self.non_symbol_subs
+
+        self.data = {}
+        self.keys = {}
+        self.MAX_TABLE_LEN = 50
 
     def __generate_nonce(self):
         return str(int(round(time.time()) + self.timeout))
@@ -85,11 +104,101 @@ class BitMEXWS:
             ],
         )
 
+    async def receive_message(self):
+        await asyncio.sleep(1)
+        async for message in self.ws:
+            # self.data = message
+            self.__on_message(message)
+            # print("message")
+            # await asyncio.sleep(1)
+
     async def connect(self):
         url, headers = self.get_url_auth()
+        self.ws = await websockets.connect(url, extra_headers=headers)
+        return self.ws.open
 
-        async with websockets.connect(url, extra_headers=headers) as ws:
-            while True:
-                data = await ws.recv()
-                print(data)
+    def state(self):
+        return self.ws.state
+
+    def is_alive(self):
+        print(self.ws.state)
+        return self.ws.state == State.OPEN
+
+    async def close(self):
+        print("GRACEFULLY CLOSING THE SOCKET")
+        await self.ws.close()
+
+    # Fetch required data
+    def get_instrument(self):
+        '''Get the raw instrument data for this symbol.'''
+        # Turn the 'tickSize' into 'tickLog' for use in rounding
+        instrument = self.data['instrument'][0]
+        instrument['tickLog'] = int(math.fabs(math.log10(instrument['tickSize'])))
+        return instrument
+
+    
+    def funds(self):
+        '''Get your margin details.'''
+        return self.data['margin'][0]
+
+    
+
+    def __on_message(self, message):
+        """Handler for parsing WS messages."""
+        message = json.loads(message)
+
+        table = message.get("table")
+        action = message.get("action")
+        try:
+            if "subscribe" in message:
+                self.logger.debug("Subscribed to %s." % message["subscribe"])
+            elif action:
+                if table not in self.data:
+                    self.data[table] = []
+
+                # There are four possible actions from the WS:
+                # 'partial' - full table image
+                # 'insert'  - new row
+                # 'update'  - update row
+                # 'delete'  - delete row
+                if action == "partial":
+                    self.data[table] = message["data"]
+                    # Keys are communicated on partials to let you know how to uniquely identify
+                    # an item. We use it for updates.
+                    self.keys[table] = message["keys"]
+                elif action == "insert":
+                    self.data[table] += message["data"]
+
+                    # Limit the max length of the table to avoid excessive memory usage.
+                    # Don't trim orders because we'll lose valuable state if we do.
+                    if (
+                        table not in ["order"]
+                        and len(self.data[table]) > self.MAX_TABLE_LEN
+                    ):
+                        self.data[table] = self.data[table][self.MAX_TABLE_LEN // 2 :]
+
+                elif action == "update":
+                    # Locate the item in the collection and update it.
+                    for updateData in message["data"]:
+                        item = find_by_keys(
+                            self.keys[table], self.data[table], updateData
+                        )
+                        if not item:
+                            return  # No item found to update. Could happen before push
+                        item.update(updateData)
+                        # Remove cancelled / filled orders
+                        if table == "order" and not order_leaves_quantity(item):
+                            self.data[table].remove(item)
+                elif action == "delete":
+                    # Locate the item in the collection and remove it.
+                    for deleteData in message["data"]:
+                        item = find_by_keys(
+                            self.keys[table], self.data[table], deleteData
+                        )
+                        self.data[table].remove(item)
+                else:
+                    raise Exception("Unknown action: %s" % action)
+        except Exception as exe:
+            # self.logger.error(traceback.format_exc())
+            print(str(exe), exe.__class__.__name__)
 
